2f2a7d0d35428ee3a4b8448d0c79313ce013bd75
Kwan Meng Keong <mengkeong@titansoft.com.sg>
2021-09-21 10:12:36 +0800

    passed one spare test

--- TitansoftTddWorkshop.UnitTests/BowlingGameTest.cs
+++ TitansoftTddWorkshop.UnitTests/BowlingGameTest.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 
 namespace TitansoftTddWorkshop.UnitTests
 {
-
     [TestFixture()]
     public class BowlingGameTest
     {
@@ -12,27 +11,35 @@ namespace TitansoftTddWorkshop.UnitTests
         [SetUp]
         public void Setup()
         {
+            _target = new BowlingGame();
         }
 
         [Test]
         public void AllZero()
         {
-            _target = new BowlingGame();
-
             RollMany(20, 0);
 
             _target.Score().Should().Be(0);
-
         }
+
         [Test]
         public void AllOne()
         {
-            _target = new BowlingGame();
-
             RollMany(20, 1);
 
             _target.Score().Should().Be(20);
+        }
 
+        [Test]
+        //[Ignore("not yet")]
+        public void OneSpare()
+        {
+            _target.Roll(5);
+            _target.Roll(5);
+            _target.Roll(2);
+            RollMany(17, 0);
+
+            _target.Score().Should().Be(14);
         }
 
         private void RollMany(int rounds, int pinsDown)
@@ -47,19 +54,37 @@ namespace TitansoftTddWorkshop.UnitTests
         public void CleanUp()
         {
         }
-
     }
 
     public class BowlingGame
     {
-        public void Roll(int i)
-        {
+        private int[] _rolls = new int[21];
+        private int current;
 
+        public void Roll(int pinsDown)
+        {
+            _rolls[current++] = pinsDown;
         }
 
         public int Score()
         {
-            return 0;
+            var score = 0;
+            var rollIndex = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (_rolls[rollIndex] + _rolls[rollIndex + 1] == 10)
+                {
+                    score += 10 + _rolls[rollIndex + 2];
+                }
+                else
+                {
+                    score += _rolls[rollIndex] + _rolls[rollIndex + 1];
+                }
+
+                rollIndex += 2;
+            }
+
+            return score;
         }
     }
 }
