65c2685366211bad5e19951cfe60a9fc6c7f5657
Kwan Meng Keong <mengkeong@titansoft.com.sg>
2021-09-25 10:46:08 +0800

    created a bowling game test class

---
+++ TitansoftTddWorkshop.UnitTests/BownlingGameTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace TitansoftTddWorkshop.UnitTests
+{
+
+    [TestFixture()]
+    public class BownlingGameTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AllZero()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+        }
+
+    }
+}
