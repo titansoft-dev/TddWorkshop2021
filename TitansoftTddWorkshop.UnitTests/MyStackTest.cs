36e2d2b76ecb95aabff5ec010840a178f7b1ffba
Kwan Meng Keong <mengkeong@titansoft.com.sg>
2021-09-25 12:27:36 +0800

    passed the 2nd exercise 1st test

---
+++ TitansoftTddWorkshop.UnitTests/MyStackTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TitansoftTddWorkshop.UnitTests
+{
+
+    [TestFixture()]
+    public class MyStackTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NewStackShouldBeEmpty()
+        {
+            //Arrange
+            int maxSize = 5;
+            var target = new MyStack<int>(maxSize);
+
+
+            //Act
+            int size = target.Size;
+
+            //Assert
+            size.Should().Be(0);
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+        }
+
+    }
+
+    public class MyStack<T>
+    {
+        public MyStack(int maxSize)
+        {
+        }
+
+        public int Size { get; set; }
+    }
+}
