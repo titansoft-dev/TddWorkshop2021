ee0c1539c925e24035760c4b9a94e8f6060abf2f
Kwan Meng Keong <mengkeong@titansoft.com.sg>
2021-09-25 12:41:53 +0800

    solution for mystack

--- TitansoftTddWorkshop.UnitTests/MyStackTest.cs
+++ TitansoftTddWorkshop.UnitTests/MyStackTest.cs
@@ -1,27 +1,17 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace TitansoftTddWorkshop.UnitTests
 {
-
-    [TestFixture()]
+    [TestFixture]
     public class MyStackTest
     {
-        private MyStack<int> _target;
-        private int _maxSize = 5;
-
         [SetUp]
         public void Setup()
         {
-        }
-
-        [Test]
-        public void NewStackShouldBeEmpty()
-        {
-
-            _target = new MyStack<int>(_maxSize);
-
-            _target.Size.Should().Be(0);
+            _target = new MyStack<int>(_StackSize);
         }
 
         [TearDown]
@@ -29,14 +19,112 @@ namespace TitansoftTddWorkshop.UnitTests
         {
         }
 
+        private MyStack<int> _target;
+        private static readonly int _StackSize = 5;
+
+        [Test]
+        public void NewStackShouldBeEmpty()
+        {
+            _target.Size.Should().Be(0);
+        }
+
+        [Test]
+        public void SizeIncreaseWhenPushItem()
+        {
+            _target.Push(10);
+
+            _target.Size.Should().Be(1);
+        }
+
+        [Test]
+        public void SizeReduceAndGetLastestItemWhenPop()
+        {
+            _target.Push(10);
+            _target.Push(20);
+
+            _target.Pop().Should().Be(20);
+
+            _target.Size.Should().Be(1);
+        }
+
+        [Test]
+        public void GetLastestItemWithoutSizeChangedWhenPeep()
+        {
+            _target.Push(10);
+            _target.Push(20);
+
+            _target.Peep().Should().Be(20);
+
+            _target.Size.Should().Be(2);
+        }
+
+        [Test]
+        public void OverPushShouldThrowStackFullException()
+        {
+            OverPush().Should().Throw<StackFullException>();
+
+        }
+
+        private Action OverPush()
+        {
+            return () =>
+            {
+                for (int i = 0; i < _StackSize + 1; i++)
+                {
+                    _target.Push(10);
+                }
+
+            };
+        }
+    }
+
+    public class StackFullException : Exception
+    {
     }
 
     public class MyStack<T>
     {
+        private readonly int _maxSize;
+        private readonly List<T> _items;
+
+        private int CurrentItemIndex => _items.Count - 1;
+
+        public int Size => _items.Count;
+
         public MyStack(int maxSize)
         {
+            _maxSize = maxSize;
+            _items = new List<T>(maxSize);
         }
 
-        public int Size { get; set; }
+
+        public void Push(T item)
+        {
+            if (IsStackFull())
+            {
+                throw new StackFullException();
+            }
+
+            _items.Add(item);
+        }
+
+        private bool IsStackFull()
+        {
+            return _items.Count == _maxSize;
+        }
+
+        public T Pop()
+        {
+            var output = _items[CurrentItemIndex];
+
+            _items.RemoveAt(CurrentItemIndex);
+
+            return output;
+        }
+
+        public T Peep()
+        {
+            return _items[CurrentItemIndex];
+        }
     }
 }
